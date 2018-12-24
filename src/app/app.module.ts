9a9082ec1199a70de6c0d796c3db51d4e191ad9a
lmjanssens <s1110698@student.hsleiden.nl>
2018-12-24 12:44:09 +0100

    Added necessary components, added base routing, separated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {LoginComponent} from './login/login.component';
+import {LoginComponent} from './Components/login/login.component';
 import {SharedModule} from './material-data/shared/shared.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavbarComponent} from './Components/navbar/navbar.component';
+import {ProductComponent} from './Components/product/product.component';
+import {ErrorComponent} from './Components/error/error.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent,
+    ProductComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
