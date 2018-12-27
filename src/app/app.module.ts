dc4e3eed0b7410da36445f436e5dcabedfab2c07
lmjanssens <s1110698@student.hsleiden.nl>
2018-12-27 14:48:50 +0100

    ga weg error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {LoginComponent} from './components/login/login.component';
+import {LoginComponent} from './Components/login/login.component';
 import {SharedModule} from './material-data/shared/shared.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {ProductComponent} from './components/product/product.component';
-import {ErrorComponent} from './components/error/error.component';
+import {NavbarComponent} from './Components/navbar/navbar.component';
+import {ProductComponent} from './Components/product/product.component';
+import {ErrorComponent} from './Components/error/error.component';
 import {AppRoutingModule} from './app-routing.module';
 import {LayoutModule} from '@angular/cdk/layout';
 import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
